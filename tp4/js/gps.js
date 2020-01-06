60f5d89cf0d7201bd51d96f91f41baf1c2735e99
koffiavumadi <56545297+koffiavumadi@users.noreply.github.com>
2020-01-06 23:25:24 +0100

    Update gps.js

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -20,7 +20,23 @@ function showPosition(position) {
 }
 
 //
-
+// question 3 Geolocalisation 
+   // $("#adresse").val(latlon);
+function getReverseGeocodingData(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    // This is making the Geocode request
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status !== google.maps.GeocoderStatus.OK) {
+            alert(status);
+        }
+        // This is checking to see if the Geoeode Status is OK before proceeding
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results);
+            var address = (results[0].formatted_address);
+        }
+    });
+}
 //
 
 // Au cas ou l'utilisateur refuse
