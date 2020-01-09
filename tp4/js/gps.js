b9f614f2eebd2deb17e5de91f6a02ad8e242cdee
koffiavumadi <56545297+koffiavumadi@users.noreply.github.com>
2020-01-09 03:29:51 +0100

    Update gps.js

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -31,9 +31,10 @@ function getReverseGeocodingData(lat, lng) {
         if (status == google.maps.GeocoderStatus.OK) {
             console.log(results);
             var address = (results[0].formatted_address);
+            $("#adresse").val(results[0].formatted_address);
         }
     });
-    $("#adresse").val(address);
+    
 }
 
 
