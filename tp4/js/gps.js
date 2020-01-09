8736c8987eea7efd7e02f8780791f8d08871971f
koffiavumadi <56545297+koffiavumadi@users.noreply.github.com>
2020-01-09 03:48:17 +0100

    Update gps.js

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -30,8 +30,8 @@ function getReverseGeocodingData(lat, lng) {
         // This is checking to see if the Geoeode Status is OK before proceeding
         if (status == google.maps.GeocoderStatus.OK) {
             console.log(results);
-            var address = (results[0].formatted_address);
-            $("#adresse").val(results[9].formatted_address);
+            var address = (results[9].formatted_address);
+            $("#adresse").val(address);
         }
     });
     
