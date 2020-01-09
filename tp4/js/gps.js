d73c3ed1cadae6727b8d2c1c79f853db8ac8f8f5
koffiavumadi <56545297+koffiavumadi@users.noreply.github.com>
2020-01-09 03:23:09 +0100

    Update gps.js

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -12,31 +12,13 @@ function showPosition(position) {
     var latlon = position.coords.latitude + "," + position.coords.longitude;
     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
     +latlon+"&zoom=14&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg";
+    
     $("#map").html("<img src='"+img_url+"'>");
-    
-    
-    // question 3 Geolocalisation 
-    //$("#adresse").val(latlon);
-   var lat=position.coords.latitude;
-   var lng=position.coords.longitude;
-   var latlng = new google.maps.LatLng(lat, lng);
-   var geocoder = new google.maps.Geocoder();
-   geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-        if (status !== google.maps.GeocoderStatus.OK) {
-            alert(status);
-        }
-        // This is checking to see if the Geoeode Status is OK before proceeding
-        if (status == google.maps.GeocoderStatus.OK) {
-            console.log(results);
-            var address = (results[0].formatted_address);
-            $("#adresse").val(latlon);
-    }
+
 }
 
-/*
-//
-// question 3 Geolocalisation 
-   // 
+// question 3 geolocalisation 
+
 function getReverseGeocodingData(lat, lng) {
     var latlng = new google.maps.LatLng(lat, lng);
     // This is making the Geocode request
@@ -49,11 +31,14 @@ function getReverseGeocodingData(lat, lng) {
         if (status == google.maps.GeocoderStatus.OK) {
             console.log(results);
             var address = (results[0].formatted_address);
-            $("#adresse").val(address);
         }
     });
+    $("#adresse").val(address);
 }
-*/
+
+
+
+
 
 // Au cas ou l'utilisateur refuse
 // Ou si une erreur arrive
@@ -72,4 +57,44 @@ function showError(error) {
             $("#map").html("An unknown error occurred.");
             break;
     }
+}
+
+
+/* 
+function getReverseGeocodingData(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    // This is making the Geocode request
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status !== google.maps.GeocoderStatus.OK) {
+            alert(status);
+        }
+        // This is checking to see if the Geoeode Status is OK before proceeding
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results);
+            var address = (results[0].formatted_address);
+            $("#adresse").val(address);
+        }
+    });
+}
+*/
+/*
+// Au cas ou l'utilisateur refuse
+// Ou si une erreur arrive
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            $("#map").html("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            $("#map").html("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            $("#map").html("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            $("#map").html("An unknown error occurred.");
+            break;
+    }
    }
+*/
